7c9ee4453c5534c1ac80827a03c3c0395f716919
Diana Malyk <d.malyk@mobidev.biz>
2022-05-20 21:36:38 +0300

    ex 5

--- autocipher/parsing.py
+++ autocipher/parsing.py
@@ -6,7 +6,7 @@ from argparse import ArgumentParser, Namespace
 def add_train_args(parser: ArgumentParser):
     parser.add_argument('--run_id',
                         type=str,
-                        default='4_nt_enc_resdec',
+                        default='5_nt_enc_resdec',
                         help='Run ID')
     parser.add_argument('--device',
                         type=str,
